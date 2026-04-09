4a383ea60ed3c53ae56306cf9f3fb3bf06832e1a
Gouacem <gouacemt@gmail.com>
2026-04-09 11:21:42 +0200

    config dossier vote

---
+++ contracts/Vote.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Vote {
+
+    address public admin;
+
+    struct Candidate {
+        string name;
+        uint voteCount;
+    }
+
+    Candidate[] public candidates;
+
+    mapping(address => bool) public hasVoted;
+    mapping(address => bool) public isVoter;
+
+    bool public votingOpen;
+    
+    event VoterAdded(address voter);
+    event VotingOpened();
+    event VotingClosed();
+    event Voted(address voter, uint candidateIndex);
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not admin");
+        _;
+    }
+    
+    function addCandidate(string memory _name) public onlyAdmin {
+        candidates.push(Candidate(_name, 0));
+    }
+
+    function addVoter(address _voter) public onlyAdmin {
+        isVoter[_voter] = true;
+        emit VoterAdded(_voter);
+    }
+
+    modifier onlyVoter() {
+        require(isVoter[msg.sender], "Not authorized");
+        _;
+    }
+    modifier votingIsOpen() {
+        require(votingOpen, "Voting closed");
+        _;
+    }
+
+    function vote(uint _candidateIndex) public onlyVoter {    
+        require(!hasVoted[msg.sender], "You already voted");
+
+        hasVoted[msg.sender] = true;
+        candidates[_candidateIndex].voteCount++;
+        emit Voted(msg.sender, _candidateIndex);
+    }
+
+    function getCandidatesCount() public view returns (uint) {
+        return candidates.length;
+    }
+
+    function openVoting() public onlyAdmin {
+        votingOpen = true;
+        emit VotingOpened();
+    }
+
+    function closeVoting() public onlyAdmin {
+        votingOpen = false;
+        emit VotingClosed();
+    }
+
+    function getWinner() public view returns (uint) {
+    uint winnerIndex = 0;
+
+    for (uint i = 1; i < candidates.length; i++) {
+        if (candidates[i].voteCount > candidates[winnerIndex].voteCount) {
+            winnerIndex = i;
+        }
+    }
+
+    return winnerIndex;
+    }
+
+}
