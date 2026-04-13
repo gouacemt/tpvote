eb3cb5baa5c39e606fb44ff8beb6d5858f7edac7
Gouacem <gouacemt@gmail.com>
2026-04-13 14:10:39 +0200

    Test 1 : déploiement du contrat Vote

--- test/Vote.ts
+++ test/Vote.ts
@@ -1,41 +1,44 @@
 import { expect } from "chai";
-import hre from "hardhat";
+import { network } from "hardhat";
+
+// Fixture de déploiement
+async function deployVoteFixture() {
+  const { ethers } = await network.connect();
+  const [admin, voter1, voter2] = await ethers.getSigners();
+
+  const VoteFactory = await ethers.getContractFactory("Vote");
+  const vote = await VoteFactory.deploy();
+  await vote.waitForDeployment();
+
+  await vote.addCandidate("Alice");
+  await vote.addCandidate("Bob");
+
+  return { vote, admin, voter1, voter2 };
+}
 
 describe("Vote", function () {
 
-  async function deployFixture() {
-    const [owner, voter1, voter2, stranger] = await hre.ethers.getSigners();
-
-    const Vote = await hre.ethers.getContractFactory("Vote");
-    const vote = await Vote.deploy();
-
-    await vote.addCandidate("Alice");
-    await vote.addCandidate("Bob");
-    await vote.addCandidate("Charlie");
-
-    return { vote, owner, voter1, voter2, stranger };
-  }
-
   describe("Déploiement", function () {
 
-    it("Le owner est bien défini", async function () {
-      const { vote, owner } = await deployFixture();
-      expect(await vote.admin()).to.equal(owner.address);
+    it("Les candidats sont-ils initialisés ?", async function () {
+      const { vote } = await deployVoteFixture();
+      const count = await vote.getCandidatesCount();
+      expect(count).to.equal(2n);
     });
 
-    it("Les candidats sont initialisés (3 candidats)", async function () {
-      const { vote } = await deployFixture();
-      expect(await vote.getCandidatesCount()).to.equal(3);
-    });
-
-    it("Tous les candidats ont 0 vote au départ", async function () {
-      const { vote } = await deployFixture();
-      for (let i = 0; i < 3; i++) {
+    it("Ont-ils tous 0 vote au départ ?", async function () {
+      const { vote } = await deployVoteFixture();
+      const count = await vote.getCandidatesCount();
+      for (let i = 0n; i < count; i++) {
         const candidate = await vote.candidates(i);
-        expect(candidate.voteCount).to.equal(0);
+        expect(candidate.voteCount).to.equal(0n);
       }
     });
 
-  });
+    it("Le owner est-il bien défini ?", async function () {
+      const { vote, admin } = await deployVoteFixture();
+      expect(await vote.admin()).to.equal(admin.address);
+    });
 
+  });
 });
